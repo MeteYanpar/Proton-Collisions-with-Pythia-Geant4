cff99643ea64148fd3d253da5618854cf6dafa87
Mete Yanpar <yanpar19@itu.edu.tr>
2025-08-11 17:02:16 +0300

    Add files via upload

---
+++ Geant4/build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/meteyanpar/work/B11/exampleB1.cc" "CMakeFiles/exampleB1.dir/exampleB1.cc.o" "gcc" "CMakeFiles/exampleB1.dir/exampleB1.cc.o.d"
+  "/home/meteyanpar/work/B11/src/ActionInitialization.cc" "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o.d"
+  "/home/meteyanpar/work/B11/src/DetectorConstruction.cc" "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/EventAction.cc" "CMakeFiles/exampleB1.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/EventAction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/Materials.cc" "CMakeFiles/exampleB1.dir/src/Materials.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/Materials.cc.o.d"
+  "/home/meteyanpar/work/B11/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/RunAction.cc" "CMakeFiles/exampleB1.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/RunAction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/SteppingAction.cc" "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/TrackingAction.cc" "CMakeFiles/exampleB1.dir/src/TrackingAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/TrackingAction.cc.o.d"
+  "/home/meteyanpar/work/B11/src/Trajectory.cc" "CMakeFiles/exampleB1.dir/src/Trajectory.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/Trajectory.cc.o.d"
+  "/home/meteyanpar/work/B11/src/TrajectoryPoint.cc" "CMakeFiles/exampleB1.dir/src/TrajectoryPoint.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/TrajectoryPoint.cc.o.d"
+  "/home/meteyanpar/work/B11/src/UserTrackInformation.cc" "CMakeFiles/exampleB1.dir/src/UserTrackInformation.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/UserTrackInformation.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
